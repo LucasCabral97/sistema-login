b5523a68c38c647c427d7839df0871ca020e153a
Lucas Cabral da Silva <lcabral97@outlook.com>
2025-05-14 22:47:20 -0300

    configurando confirmação email

--- confirmacao.php
+++ confirmacao.php
@@ -1,25 +1,25 @@
 <?php
     require('config/conexao.php');
-    if(isset($_GET['cod_confirm'])){
+    if(isset($_GET['cod_confirm']) && !empty($_GET['cod_confirm'])){
 
-        $confirmacao = $_GET['cod_confirm'];
+        $confirmacao = limparPost($_GET['cod_confirm']);
         $sql=$pdo->prepare("SELECT * FROM usuarios WHERE codigo_confirmacao=? LIMIT 1");
         $sql->execute(array($confirmacao));
 
-        $user = $sql->fetch();
+        $user = $sql->fetch(PDO::FETCH_ASSOC);
 
-        if(!$user){
-            echo "Usuário não encontrado!";    
-        }else{
-
+        if($user){
+            
             $status = "confirmado";
             $sql=$pdo->prepare("UPDATE usuarios SET status=? WHERE codigo_confirmacao=?");
             $sql->execute(array($status,$confirmacao));
+            
+            header("location: index.php");
 
+        }else{
+            echo "<h1>Código de confirmação inválido!</h1>";
         }
-
     }else{
         header("location: index.php");
     }
-
 ?>
